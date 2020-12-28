6fc74003ae982dce940ec9f76a3b000ad3dde525
BBUBBABBABBU <gksrhkddnd1@naver.com>
2020-12-29 05:58:16 +0900

    광웅 12.28 푸쉬

--- zzTourr/src/main/java/com/zzTourr/controller/HotelController.java
+++ zzTourr/src/main/java/com/zzTourr/controller/HotelController.java
@@ -6,14 +6,51 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HotelController {
+	@RequestMapping("hotelindex.trip")
+	public String index() {
+		return "hotelViews/hotellogin";
+	}
 	
 	@RequestMapping("ownerlogin.trip")
 	public String login(String ownerId, String ownerPass ) {
-		return "Hotelview/hotelmain";
-	}
-	@RequestMapping("newOwner.trip")
-	public String newOwner() {
-		return "Hotelview/hotelNewOwner";
+		return "hotelViews/hotelmain";
 	}
 	
+	@RequestMapping("newOwner.trip")
+	public String newOwner() {
+		return "hotelViews/hotelNewOwner";
+	}
+	
+	@RequestMapping("newLodge.trip")
+	public String newLodge() {
+		return "hotelViews/hotelNewLodge";
+	}
+	
+	@RequestMapping("ModifyLodge.trip")
+	public String ModifyLodge() {
+		return "hotelViews/hotelModifyLodge";
+	}
+	
+	@RequestMapping("salHis.trip")
+	public String salHis() {
+		return "hotelViews/hotelSalHis";
+	}
+	
+	@RequestMapping("ResMgr.trip")
+	public String resMgr() {
+		return "hotelViews/hotelResMgr";
+	}
+	
+	@RequestMapping("priceEdit.trip")
+	public String priceEdit() {
+		return "hotelViews/hotelPriceEdit";
+	}
+	@RequestMapping("Review.trip")
+	public String review() {
+		return "hotelViews/hotelReview";
+	}
+	@RequestMapping("Mypage.trip")
+	public String Mypage() {
+		return "hotelViews/hotelMypage";
+	}
 }
--- zzTourr/src/main/java/com/zzTourr/dao/HotelDAO.java
+++ zzTourr/src/main/java/com/zzTourr/dao/HotelDAO.java
@@ -1,5 +1,12 @@
 package com.zzTourr.dao;
 
-public interface HotelDAO {
+import com.zzTourr.domain.MgrVO;
 
+public interface HotelDAO {
+	
+	int hotelMgrInsert(MgrVO vo);
+	
+	MgrVO hotelMgrlogin(MgrVO vo);
+	
+	
 }
